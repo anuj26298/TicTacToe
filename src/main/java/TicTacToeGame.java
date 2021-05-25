b5074834f72c95eec058a0d029514420f207aeda
anuj26298 <kumar26298anuj@gmail.com>
2021-05-25 12:14:51 +0530

    ADDED user and computer choice

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class TicTacToeGame {
+    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Welcome to Tic-Tac-Toe");
+        char[] board = createBoard();
+
+        String computerChoice = enterInput();
+    }
+
+    static char[] createBoard(){
+        char[] board = new char[10];
+        for(int i=1; i<=10;i++){
+            board[i] = '-';
+        }
+        return board;
+
+    }
+    static String enterInput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose x or o");
+        String userChoice = scanner.next();
+        String computer;
+        if(userChoice != "x" || userChoice != "o"  ){
+            System.out.println("Enter valid Input");
+            enterInput();
+        }
+        if(userChoice.equals('x'))
+            computer = "o";
+        else
+            computer = "x";
+
+        return computer;
     }
 }
