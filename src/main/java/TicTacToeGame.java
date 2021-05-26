6543efd64d5419d2d0f63dc31ed1e9309c9d80ad
anuj26298 <kumar26298anuj@gmail.com>
2021-05-26 16:53:52 +0530

    none

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -1,27 +1,5 @@
-import java.util.Scanner;
-
 public class TicTacToeGame {
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
         System.out.println("Welcome to Tic-Tac-Toe");
-        char[] board = createBoard();
-        char userLetter = enterInput(userInput);
-        char computerLetter = (userLetter == 'X') ? 'O' : 'X';
     }
-
-    private static char[] createBoard(){
-        char[] board = new char[10];
-        for(int i=1; i<=10;i++){
-            board[i] = ' ';
-        }
-        return board;
-
-    }
-    private static char enterInput(Scanner userInput){
-        System.out.println("Choose your letter(X or O");
-        return userInput.next().toUpperCase().charAt(0);
-    }
-
-   
-
 }
