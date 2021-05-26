535c4b2ad3ff606573570ea53982ee51f8f2f74d
anuj26298 <kumar26298anuj@gmail.com>
2021-05-26 17:28:56 +0530

    ADDED showBoard

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -1,59 +1,38 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-    private static char[] board;
-
-    //    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
         System.out.println("Welcome to Tic-Tac-Toe");
         char[] board = createBoard();
-        char userLetter = enterInput(userInput);
-        char computerLetter = {userLetter == 'X'} ? 'O' : 'X';
+        Scanner userInput = new Scanner(System.in);
+        char userLetter = chooseUserLetter(userInput);
 
-        char computerChoice = enterInput();
+        char computerLetter = (userLetter == 'X') ? 'O' : 'X';
+
+        System.out.println("User Letter: " + userLetter);
+        System.out.println("Computer Letter: " + computerLetter);
+        showBoard(board);
     }
 
-    private static char enterInput() {
-        return 0;
-    }
-
-    static char[] createBoard(){
+    private static char[] createBoard() {
         char[] board = new char[10];
-        for(int i=1; i<=10;i++){
+        for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
         return board;
-
     }
-    private static char enterInput(Scanner userInput){
-//        Scanner scanner = new Scanner(System.in);
-             System.out.println("Choose x or o");
-//        String userChoice = scanner.next();
-//        String computer;
-//        if(userChoice != "x" || userChoice != "o"  ){
-//            System.out.println("Enter valid Input");
-//            enterInput();
-//        }
-//        if(userChoice.equals('x'))
-//            computer = "o";
-//        else
-//            computer = "x";
-//
+
+    private static char chooseUserLetter(Scanner userInput) {
+        System.out.println("Choose Your Letter(X or O)");
         return userInput.next().toUpperCase().charAt(0);
     }
 
-    static void showBoard(){
-        System.out.println("|---|---|---|");
-
-        System.out.println("| " + board[1]  + " | " +
+    private static void showBoard(char[] board) {
+        System.out.println("| " + board[1] + " | " +
                 board[2] + " | " + board[3] + " | ");
-
-        System.out.println("| " + board[4]  + " | " +
+        System.out.println("| " + board[4] + " | " +
                 board[5] + " | " + board[6] + " | ");
-
-        System.out.println("| " + board[7]  + " | " +
+        System.out.println("| " + board[7] + " | " +
                 board[8] + " | " + board[9] + " | ");
     }
 
