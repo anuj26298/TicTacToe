375f0ef973915d16c9ec4fea5dfea30e8a8daa0b
anuj26298 <kumar26298anuj@gmail.com>
2021-05-26 17:02:00 +0530

    ADDED createBoard method

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -1,5 +1,15 @@
 public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Tic-Tac-Toe");
+        char[] board = createBoard();
     }
+
+    private static char[] createBoard(){
+        char[] board = new char[10];
+        for (int i=1;i< board.length; i++){
+            board[i] = ' ';
+        }
+        return board;
+    }
+
 }
