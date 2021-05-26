76b15d298f35ceae51e7b9569baaaf1f236c6cc0
anuj26298 <kumar26298anuj@gmail.com>
2021-05-26 17:42:32 +0530

    ADDED taken user move and validating move

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -1,78 +1,53 @@
-import java.util.Locale;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-    private static char[] board;
-
-    //    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
         System.out.println("Welcome to Tic-Tac-Toe");
         char[] board = createBoard();
-        char userLetter = enterInput(userInput);
+        Scanner userInput = new Scanner(System.in);
+        char userLetter = chooseUserLetter(userInput);
+
         char computerLetter = (userLetter == 'X') ? 'O' : 'X';
-        showBoard();
-        char computerChoice = enterInput();
+
+        System.out.println("User Letter: " + userLetter);
+        System.out.println("Computer Letter: " + computerLetter);
+        showBoard(board);
+        int userMove = getUserMove(board, userInput);
     }
 
-    private static char enterInput() {
-        return 0;
-    }
-
-    static char[] createBoard() {
+    private static char[] createBoard() {
         char[] board = new char[10];
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
         return board;
-
     }
 
-    private static char enterInput(Scanner userInput) {
-//        Scanner scanner = new Scanner(System.in);
-        System.out.println("Choose x or o");
-//        String userChoice = scanner.next();
-//        String computer;
-//        if(userChoice != "x" || userChoice != "o"  ){
-//            System.out.println("Enter valid Input");
-//            enterInput();
-//        }
-//        if(userChoice.equals('x'))
-//            computer = "o";
-//        else
-//            computer = "x";
-//
+    private static char chooseUserLetter(Scanner userInput) {
+        System.out.println("Choose Your Letter(X or O)");
         return userInput.next().toUpperCase().charAt(0);
     }
 
-    static void showBoard() {
-        System.out.println("|---|---|---|");
-
+    private static void showBoard(char[] board) {
         System.out.println("| " + board[1] + " | " +
                 board[2] + " | " + board[3] + " | ");
-
         System.out.println("| " + board[4] + " | " +
                 board[5] + " | " + board[6] + " | ");
-
         System.out.println("| " + board[7] + " | " +
                 board[8] + " | " + board[9] + " | ");
     }
 
-    int userMove() {
-        System.out.println("Enter your move(Between 1 to 9)");
-        Scanner scanner = new Scanner(System.in);
-        int userMove = scanner.nextInt();
-        if (board[userMove] == ' ')
-
-            return userMove;
-        else{
-            System.out.println("Invalid Move: Please play a valid move..." +
-                    "move should be other than " + userMove);
-            userMove();
-            return 0;
+    private static int getUserMove(char[] board, Scanner userInput){
+        Integer[] validCells = {1,2,3,4,5,6,7,8,9};
+        while (true) {
+            System.out.println("Enter your next move(1-9)");
+            int index = userInput.nextInt();
+            if (Arrays.asList(validCells).contains(index) && isSpaceFree(board, index))
+                return index;
         }
-
-
     }
-
+    private static boolean isSpaceFree(char[] board, int index){
+        return board[index] == ' ';
+    }
 }
