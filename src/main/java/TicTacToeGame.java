f6be6c7c2b49be2da4f1871b45f9aa38c2057145
anuj26298 <kumar26298anuj@gmail.com>
2021-05-26 16:35:49 +0530

    REFACTORED code

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -1,40 +1,24 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-//    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Welcome to Tic-Tac-Toe");
         char[] board = createBoard();
         char userLetter = enterInput(userInput);
-        char computerLetter = {userLetter == 'X'} ? 'O' : 'X';
-
-        String computerChoice = enterInput();
+        char computerLetter = (userLetter == 'X') ? 'O' : 'X';
     }
 
-    static char[] createBoard(){
+    private static char[] createBoard(){
         char[] board = new char[10];
         for(int i=1; i<=10;i++){
-            board[i] = '-';
+            board[i] = ' ';
         }
         return board;
 
     }
     private static char enterInput(Scanner userInput){
-//        Scanner scanner = new Scanner(System.in);
-             System.out.println("Choose x or o");
-//        String userChoice = scanner.next();
-//        String computer;
-//        if(userChoice != "x" || userChoice != "o"  ){
-//            System.out.println("Enter valid Input");
-//            enterInput();
-//        }
-//        if(userChoice.equals('x'))
-//            computer = "o";
-//        else
-//            computer = "x";
-//
+        System.out.println("Choose your letter(X or O");
         return userInput.next().toUpperCase().charAt(0);
     }
 
