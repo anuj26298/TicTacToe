115282cf84cd0e0854ca2663b89b96731f44f5bc
anuj26298 <kumar26298anuj@gmail.com>
2021-05-26 17:14:16 +0530

    ADDED User and Computer Letter Method

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Tic-Tac-Toe");
+        char[] board = createBoard();
+        Scanner userInput = new Scanner(System.in);
+        char userLetter = chooseUserLetter(userInput);
+
+        char computerLetter = (userLetter == 'X') ? 'O' : 'X';
+
+        System.out.println("User Letter: " + userLetter);
+        System.out.println("Computer Letter: " + computerLetter);
     }
+
+    private static char[] createBoard(){
+        char[] board = new char[10];
+        for (int i=1;i< board.length; i++){
+            board[i] = ' ';
+        }
+        return board;
+    }
+
+    private static char chooseUserLetter(Scanner userInput){
+        System.out.println("Choose Your Letter(X or O)");
+        return userInput.next().toUpperCase().charAt(0);
+    }
+
+
 }
